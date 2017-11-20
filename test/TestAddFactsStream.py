8487f5cdbd33c67fd7a150ae59f609c14d6b3cbf
Dilpesh Jain <djain16aug@gmail.com>
2017-11-20 18:12:56 +0530

    refs Falkonry/tracker#814

--- test/TestAddFactsStream.py
+++ test/TestAddFactsStream.py
@@ -3,7 +3,9 @@ import unittest
 import random
 
 host  = 'https://localhost:8080'  # host url
-token = '2mxtm6vaor8m4klbmh4zhn80khsji74y'                       # auth token
+# token = '2mxtm6vaor8m4klbmh4zhn80khsji74y'                       # auth token
+
+
 
 
 #Add facts for single thing datastream
@@ -109,7 +111,6 @@ class TestAddFacts(unittest.TestCase):
                     'endTimeIdentifier': "end",
                     'timeFormat': "iso_8601",
                     'timeZone': time.get_zone(),
-                    # 'entityIdentifier': "car",
                     'valueIdentifier': "Health"
                 }
 
@@ -131,6 +132,125 @@ class TestAddFacts(unittest.TestCase):
             print(e.message)
             self.assertEqual(0,1,"Cannot add data")
 
+    # Add facts data (csv format) with tags from a stream to  Assessment
+    def test_add_csv_facts_with_tags(self):
+        fclient = FClient(host=host, token=token,options=None)
+        datastream = Schemas.Datastream()
+        datastream.set_name('Motor Health' + str(random.random()))
+
+        datasource = Schemas.Datasource()
+        field = Schemas.Field()
+        time = Schemas.Time()
+        signal = Schemas.Signal()
+
+        time.set_zone("GMT")
+        time.set_identifier("time")
+        time.set_format("YYYY-MM-DD HH:mm:ss")
+        field.set_signal(signal)
+        datasource.set_type("STANDALONE")
+        field.set_time(time)
+        datastream.set_datasource(datasource)
+        datastream.set_field(field)
+        try:
+            datastreamResponse = fclient.create_datastream(datastream)
+            data = '{"time" :"2016-03-01 01:01:01", "current" : 12.4, "vibration" : 3.4, "state" : "On"}'
+            response = fclient.add_input_data(datastreamResponse.get_id(), 'json', {}, data)
+
+            asmtRequest = Schemas.AssessmentRequest()
+            asmtRequest.set_name('Assessment Name ' + str(random.random()))
+            asmtRequest.set_datastream(datastreamResponse.get_id())
+            asmtRequest.set_rate('PT0S')
+
+            try:
+                resp_assessment = fclient.create_assessment(asmtRequest)
+                data = io.open('./test/factsDataWithTags.csv')
+
+                options = {
+                    'startTimeIdentifier': "time",
+                    'endTimeIdentifier': "end",
+                    'timeFormat': "iso_8601",
+                    'timeZone': time.get_zone(),
+                    'valueIdentifier': "Health",
+                    'tagIdentifier': "Tags"
+                }
+
+
+                response = fclient.add_facts_stream(resp_assessment.get_id(), 'csv', options, data)
+                # tear down
+                try:
+                    fclient.delete_assessment(resp_assessment.get_id())
+                    fclient.delete_datastream(datastreamResponse.get_id())
+                except Exception as e:
+                    pass
+            except Exception as e:
+                try:
+                    fclient.delete_datastream(datastreamResponse.get_id())
+                except Exception as e:
+                    pass
+                self.assertEqual(0, 1, 'Cannot create assessment')
+        except Exception as e:
+            print(e.message)
+            self.assertEqual(0,1,"Cannot add data")
+
+    # Add facts data (csv format) with additional Tag from a stream to  Assessment
+    def test_add_csv_facts_with_additional_tags(self):
+        fclient = FClient(host=host, token=token, options=None)
+        datastream = Schemas.Datastream()
+        datastream.set_name('Motor Health' + str(random.random()))
+
+        datasource = Schemas.Datasource()
+        field = Schemas.Field()
+        time = Schemas.Time()
+        signal = Schemas.Signal()
+
+        time.set_zone("GMT")
+        time.set_identifier("time")
+        time.set_format("YYYY-MM-DD HH:mm:ss")
+        field.set_signal(signal)
+        datasource.set_type("STANDALONE")
+        field.set_time(time)
+        datastream.set_datasource(datasource)
+        datastream.set_field(field)
+        try:
+            datastreamResponse = fclient.create_datastream(datastream)
+            data = '{"time" :"2016-03-01 01:01:01", "current" : 12.4, "vibration" : 3.4, "state" : "On"}'
+            response = fclient.add_input_data(datastreamResponse.get_id(), 'json', {}, data)
+
+            asmtRequest = Schemas.AssessmentRequest()
+            asmtRequest.set_name('Assessment Name ' + str(random.random()))
+            asmtRequest.set_datastream(datastreamResponse.get_id())
+            asmtRequest.set_rate('PT0S')
+
+            try:
+                resp_assessment = fclient.create_assessment(asmtRequest)
+                data = io.open('./test/factsData.csv')
+
+                options = {
+                    'startTimeIdentifier': "time",
+                    'endTimeIdentifier': "end",
+                    'timeFormat': "iso_8601",
+                    'timeZone': time.get_zone(),
+                    'valueIdentifier': "Health",
+                    'additionalTag': "testTag"
+                }
+
+                response = fclient.add_facts_stream(resp_assessment.get_id(), 'csv', options, data)
+                # tear down
+                try:
+                    fclient.delete_assessment(resp_assessment.get_id())
+                    fclient.delete_datastream(datastreamResponse.get_id())
+                except Exception as e:
+                    pass
+            except Exception as e:
+                try:
+                    fclient.delete_datastream(datastreamResponse.get_id())
+                except Exception as e:
+                    pass
+                self.assertEqual(0, 1, 'Cannot create assessment')
+        except Exception as e:
+            print(e.message)
+            self.assertEqual(0, 1, "Cannot add data")
+
 
 if __name__ == '__main__':
     if __package__ is None:
@@ -149,3 +269,6 @@ if __name__ == '__main__':
         from ..falkonryclient import schemas as Schemas
         from ..falkonryclient import client as FClient
     unittest.main()
+else:
+    from falkonryclient import schemas as Schemas
+    from falkonryclient import client as FClient
