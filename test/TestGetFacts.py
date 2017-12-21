14e340a87a581367ae71995cff453d78189ed48a
Phagun Baya <phagun.baya@gmail.com>
2017-12-20 22:37:53 -0800

    fixing fact testcase

--- test/TestGetFacts.py
+++ test/TestGetFacts.py
@@ -3,16 +3,10 @@ import unittest
 import xmlrunner
 from pprint import pprint
 
-<<<<<<< HEAD
-host       = os.environ['FALKONRY_HOST_URL']  # host url
-token      = os.environ['FALKONRY_TOKEN']     # auth token
-assessment = 'y7dcwmp72v68tr'            # assessment id
-=======
-host  = 'https://localhost:8080'            # host url
-token = '6lt48c29d62nb4hmm2nhjwrcjwjcy76h'  # auth token
-assessment = 'pw4m44c98wmp4h'               # assessment id
->>>>>>> 4a77e2235c4d836a17baa98baa3090d48905e3c0
-
+host        = os.environ['FALKONRY_HOST_URL']  # host url
+token       = os.environ['FALKONRY_TOKEN']     # auth token
+assessment  = 'y7dcwmp72v68tr'                 # assessment id
+assessmentB = 'vpc4n96q7t994b'
 
 class TestAssessmentGetFacts(unittest.TestCase):
 
@@ -28,31 +22,29 @@ class TestAssessmentGetFacts(unittest.TestCase):
             self.assertEqual(len(response.text) == 0, False, 'Invalid facts response')
         except Exception as e:
             print(e.message)
+            self.assertEqual(0, 1, "Cannot get facts from the assessment")
 
     def test_get_assessment_facts_with_model(self):
 
         try:
-
             options = {'startTime': '2011-01-01T00:00:00.000Z', 'endTime': '2014-12-31T00:00:00.000Z', 'model': '2'}
             response = self.fclient.get_facts(assessment, options)
             pprint(response.text)
-<<<<<<< HEAD
             self.assertEqual(len(response.text) == 0, False, 'Invalid facts response')
-=======
-            self.assertEqual(len(response.text)==0,False, 'Invalid facts response')
-            print(e.message)
         except Exception as e:
+            print(e.message)
+            self.assertEqual(0, 1, "Cannot get facts from the assessment for a specific model")
 
     def test_get_assessment_facts_with_batch(self):
-        fclient = FClient(host=host, token=token, options=None)
+
         try:
-            response = fclient.get_facts(assessment, {})
+            response = self.fclient.get_facts(assessmentB, {})
             pprint(response.text)
-            self.assertEqual('batch' in response.text, True, 'Invalid batch response')
-            self.assertEqual(len(response.text)==0,False, 'Invalid facts response')
->>>>>>> 4a77e2235c4d836a17baa98baa3090d48905e3c0
+            self.assertEqual('batch' in response.text, True, 'Invalid facts with batch response')
+            self.assertEqual(len(response.text)==0,False, 'Invalid facts with batch response')
         except Exception as e:
             print(e.message)
+            self.assertEqual(0, 1, "Cannot get facts from the assessment for batch case")
 
 
 if __name__ == '__main__':
