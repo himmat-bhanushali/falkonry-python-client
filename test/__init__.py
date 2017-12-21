c1235e28de6d51f299a74275c221188cdef91c0a
Phagun Baya <phagun.baya@gmail.com>
2017-12-20 17:30:08 -0800

    token cleanup

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,3 @@
-import os
-
-host  = os.environ['FALKONRY_HOST_URL']  # host url
-token = os.environ['FALKONRY_TOKEN']
-
-
 print('Running tests....')
-print('Using:')
-print('\tFALKONRY_HOST_URL: ' + host)
-print('\tFALKONRY_TOKEN: ' + token)
+
 
