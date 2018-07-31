4003aa947238e3e1aa1208e80964303488b248b6
Phagun Baya <phagun.baya@gmail.com>
2018-07-30 20:48:51 -0700

    releasing 2.2.0

--- falkonryclient/__init__.py
+++ falkonryclient/__init__.py
@@ -24,7 +24,7 @@ __all__ = [
   'schemas'
 ]
 __title__     = 'falkonryclient'
-__version__   = '2.1.1'
+__version__   = '2.2.0'
 __author__    = 'Falkonry Inc'
 __copyright__ = 'Copyright 2016-2018 Falkonry Inc'
 __license__   = """
